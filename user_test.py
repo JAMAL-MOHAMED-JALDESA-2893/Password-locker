232d95d1b4ebdda8c8e965b74541c9ca6a308451
JAMAL MOHAMED <jamal.mohamed@student.moringaschool.com>
2021-09-05 12:31:00 +0300

    created a TestUser class to help in creating test cases in user_test

--- user_test.py
+++ user_test.py
@@ -1,2 +1,12 @@
 from user import User  # imported User class from user module
 import unittest        # imported in-built unnitest module
+
+class TestUser(unittest.TestCase): 
+    '''      
+        unittest.TestCase: TestCase class that helps in creating test cases
+    '''
+    def setUp(self):
+        '''
+        set up method to run before the test case
+        '''
+        self.new_user = User("Jamal", "jayhamo22@gmail.com", "sicario2793")
