89626be6c84f0b7c6dcff152915bd2c8cd579777
JAMAL MOHAMED <jamal.mohamed@student.moringaschool.com>
2021-09-05 14:47:13 +0300

    imported unnitest module

--- credential_test.py
+++ credential_test.py
@@ -1,2 +1,3 @@
 from credential import Credential  # Importing Credential Class from credential module
-
+import unittest                    # Importing an inbuilt unittest Module for unit testing
+ 
