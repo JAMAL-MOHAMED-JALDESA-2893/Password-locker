9201ebc4a19630cbd7926f70cbc1cb1f307ae58b
JAMAL MOHAMED <jamal.mohamed@student.moringaschool.com>
2021-09-05 14:19:38 +0300

    created Credential class in credential file : also created constructor method def__ini__

---
+++ credential.py
@@ -0,0 +1,14 @@
+# Class Credentials class that create new instance of credential
+class Credential:
+
+    credential_list = [] # Empty list: credential static variable
+
+    def __init__(self, first_name, last_name, user_name, password, email):
+        '''
+        __init__ method help us define the property of our object        
+        '''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.user_name = user_name
+        self.password = password
+        self.email = email
