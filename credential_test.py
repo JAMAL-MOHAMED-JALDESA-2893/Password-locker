bc6936066e6313810b7133d66bfbc057df9d1884
JAMAL MOHAMED <jamal.mohamed@student.moringaschool.com>
2021-09-05 15:21:19 +0300

    created a class TestCredential and methods setUp and test_init

--- credential_test.py
+++ credential_test.py
@@ -1,3 +1,20 @@
 from credential import Credential  # Importing Credential Class from credential module
 import unittest                    # Importing an inbuilt unittest Module for unit testing
- 
+
+class TestCredential(unittest.TestCase):
+          
+    def setUp(self):
+        '''
+        Set up method to run before each test cases.
+        '''
+        self.new_credential = Credential("Jamal", "Hassan", "Jamal", "sicario2793", "jayhamo22@gmail.com") 
+
+    def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
+        self.assertEqual(self.new_credential.first_name, "Jamal")
+        self.assertEqual(self.new_credential.last_name, "Hassan")
+        self.assertEqual(self.new_credential.user_name, "Jamal")
+        self.assertEqual(self.new_credential.password, "sicario2793")
+        self.assertEqual(self.new_credential.email, "jayhamo22@gmail.com")
