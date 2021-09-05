11c2af04b1ee53dd571e3233c0686d98a735a0c7
JAMAL MOHAMED <jamal.mohamed@student.moringaschool.com>
2021-09-05 12:44:30 +0300

    added test_init and test_save_user in TestUser class

--- user_test.py
+++ user_test.py
@@ -13,6 +13,22 @@ class TestUser(unittest.TestCase):
   
     def tearDown(self):
         '''
-        tearDown method that does clean up after each test case has run.
+        tearDown method  cleans up after each test case has run.
         '''
         User.user_list = []
+
+    def test_init(self):
+        '''
+        test_init test if object utilizes proper instantiation
+        '''
+        self.assertEqual(self.new_user.user_name,"Jamal")
+        self.assertEqual(self.new_user.password,"sicario2793")
+        self.assertEqual(self.new_user.email,"jayhamo22@gmail.com")
+       
+    def test_save_user(self):
+        '''
+         tests if the user object is saved in users array
+        '''
+        self.new_user.save_user() 
+        self.assertEqual(len(User.user_list), 1)
+    
